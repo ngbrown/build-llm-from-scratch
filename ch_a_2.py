66c5c2150b2b11ca34bb02020b9ec8df22bb4257
Nathan Brown <nathan@nkbrown.us>
2024-09-27 16:00:49 -0700

    Add appendix A part 1

---
+++ ch_a_2.py
@@ -0,0 +1,10 @@
+# %%
+import torch
+
+# %%
+tensor0d = torch.tensor(1)
+tensor1d = torch.tensor([1, 2, 3])
+tensor2d = torch.tensor([[1, 2],
+                         [3, 4]])
+tensor3d = torch.tensor([[[1, 2], [3, 4]],
+                         [[5, 6], [7, 8]]])
